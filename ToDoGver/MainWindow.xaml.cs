bc4ac509ec7c90fa4d65783d6b450c11dc4cdbf0
Z9Master <34867938+Z9Master@users.noreply.github.com>
2020-06-21 11:28:11 +0200

    demo
    demo

---
+++ ToDoGver/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoGver.OtherWindows.EventsWindows;
+
+namespace ToDoGver
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FastTask ft = new FastTask();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ft.LoadFile();
+            LB_OneTimeEvent.ItemsSource = ft.ListOneTimeEvents;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            string eventIndex = (sender as CheckBox).Tag.ToString();
+            byte fail = RemoveFastTask(Int32.Parse(eventIndex));
+            if(fail == 1)
+            {
+                (sender as CheckBox).IsChecked = false;
+            }
+        }
+
+        private void BtnAddFtask_Click(object sender, RoutedEventArgs e)
+        {
+            AddFastTask();
+        }
+
+        void AddFastTask()
+        {
+            string nametask = TB_OnetimeTaskName.Text.ToString();
+            if(!(nametask.Equals("") || nametask == null))
+            {
+                ft.AddItems(nametask);
+                LB_OneTimeEvent.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Please give a name for the event", "Attention",MessageBoxButton.OK,MessageBoxImage.Information);
+            }        
+        }
+
+        byte RemoveFastTask(int eventID)
+        {
+            MessageBoxResult dr;
+            dr = MessageBox.Show("Complete task? +N/A gold + N/A diamond","Complete task",MessageBoxButton.YesNo,MessageBoxImage.Question);
+            if(dr == MessageBoxResult.Yes)
+            {
+                ft.DeleteEvent(eventID);
+                LB_OneTimeEvent.Items.Refresh();
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
+
+
+#region Old codes
+//void removeOneTimeEvent()
+//{
+
+//}
+
+//void add_event()
+//{
+//    List<OneTimeEvent> ev = new List<OneTimeEvent>();
+//    ev.Add(new OneTimeEvent() { ID = 1, event_name = "Head" });
+//    //{
+//    //    new OneTimeEvent()
+//    //    {
+//    //        event_name = "Head",
+//    //        list_subevent = new List<SubEvent>()
+//    //        {
+//    //            new SubEvent(){sub_event = "1"},
+//    //            new SubEvent(){sub_event = "2"}
+//    //        }
+//    //    }
+//    //};
+//    LB_OneTimeEvent.ItemsSource = ev;
+//    //ev.RemoveAt(0);
+//    //MessageBox.Show(ev[0].ID.ToString());
+//}
+
+
+//    public class OneTimeEvent
+//{
+//    public int ID { get; set; }
+//    public string event_name { get; set; }
+//    public List<SubEvent> list_subevent { get; set; }
+//}
+
+//public class SubEvent
+//{
+//    public string sub_event { get; set; }
+//}
+
+#endregion
---
+++ ToDoGver/OtherWindows/EventsWindows/FastTask.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace ToDoGver.OtherWindows.EventsWindows
+{
+    class FastTask
+    {
+        // Unique id for indentify one time tasks
+        private int id = 0;
+
+        // Path to the data task file
+        string filePath = @"E:\Project\C#\GverReminder\ToDoGver\ToDoGver\Data\OneTimeEvent.txt";
+        
+        // List to store task to save to the file
+        public List<OneTimeEvent> ListOneTimeEvents = new List<OneTimeEvent>();
+
+        public 
+        
+        // The list to load file text to parse to the ListOneTimeEvents
+        List<string> lines;
+        
+        // Simple load file from filePath
+        public void LoadFile()
+        {
+            try
+            {
+                lines = File.ReadAllLines(filePath).ToList();
+                foreach (var line in lines)
+                {
+                    string[] entries = line.Split(',');
+                    OneTimeEvent oneTask = new OneTimeEvent();
+
+                    oneTask.nameEvent = entries[0];
+                    oneTask.IdEvent = Int32.Parse(entries[1]);
+                    ListOneTimeEvents.Add(oneTask);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can´t load file");
+            }
+        }
+
+        // git test
+
+        // Simple delete task by IdEvent
+        public void DeleteEvent(int index)
+        {
+            List<string> output = new List<string>();
+            ListOneTimeEvents.Remove(new OneTimeEvent() { IdEvent = index });
+            foreach (var task in ListOneTimeEvents)
+            {
+                output.Add($"{task.nameEvent},{task.IdEvent}");
+            }
+            File.WriteAllLines(filePath, output);
+        }
+
+        // Simple add task with name and unique ID
+        public void AddItems(string name)
+        {
+            if(ListOneTimeEvents.Count >= 1)
+            {
+                id = ListOneTimeEvents[ListOneTimeEvents.Count() - 1].IdEvent + 1;
+            }
+            else
+            {
+                id = 0;
+            }
+            List<string> output = new List<string>();
+            ListOneTimeEvents.Add(new OneTimeEvent() { nameEvent = name, IdEvent = id });
+            foreach (var task in ListOneTimeEvents)
+            {
+                output.Add($"{task.nameEvent},{task.IdEvent}");
+            }
+            File.WriteAllLines(filePath, output);
+        }
+        
+    }
+
+    // Class that define variables for ListOneTimeEvents
+    class OneTimeEvent : IEquatable<OneTimeEvent>
+    {
+        public string nameEvent { get; set; }
+        public int IdEvent { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            OneTimeEvent objAsPart = obj as OneTimeEvent;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+
+        public override int GetHashCode()
+        {
+            return IdEvent;
+        }
+
+        public bool Equals(OneTimeEvent other)
+        {
+            if (other == null) return false;
+            return (this.IdEvent.Equals(other.IdEvent));
+        }
+    }
+}
